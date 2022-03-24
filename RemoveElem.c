e25eeafd400c66d2a055607b7515c9e5b9e30e06
Fhr2001 <fhr2001@hotmail.com>
2022-03-24 00:21:03 -0300

    Código quase pronto
    Não esta conseguindo encontrar o valor para remover

--- RemoveElem.c
+++ RemoveElem.c
@@ -5,9 +5,11 @@ typedef struct lista2 {
     int num;
     //ponteiros para o valor anterior e o próximo
     struct lista2 *ant, *prox; 
-} Lista2:
+} Lista2;
 
-Lista2* Insere (Lista* ini, int val);
+Lista2* Cria (void);
+
+Lista2* Insere (Lista2* ini, int val);
 
 Lista2* Localiza (Lista2* ini, int remov);
 
@@ -17,10 +19,45 @@ Lista2* Remove (Lista2* ini, int remov);
 
 int main (){
 
+    int op, aux;
+    Lista2* lst;
 
+    do
+    {
+        printf ("Digite 1 para inserir um termo\n");
+        printf ("Digite 2 para remover um termo\n");
+        printf ("Digite 0 para interromper o programa\n");
+
+        scanf ("%d", &op);
+
+        switch (op){
+        case 1:
+
+            printf ("Insira o numero inteiro\n");
+            scanf ("%d", &aux);
+
+            Insere (lst, aux);
+
+            break;
+        
+        case 2:
+
+            printf ("Qual número você quer remover?\n");
+            scanf ("%d", &aux);
+
+            Remove (lst, aux);
+
+            break;
+        }
+    } while (op);
+    
 }
 
-Lista2* Insere (Lista* ini, int val){
+Lista2* Cria (void){
+    return NULL;
+}
+
+Lista2* Insere (Lista2* ini, int val){
 
     Lista2* temp = (Lista2*) malloc (sizeof(Lista2));
 
@@ -29,22 +66,30 @@ Lista2* Insere (Lista* ini, int val){
     temp -> ant = NULL;
 
     //se não for vazio, ele procede (NULL == 0 == false)
-    if (lst)
-        lst->ant = temp;
+    if (ini != NULL)
+        ini -> ant = temp;
 
     return temp;
 }
 
 Lista2* Localiza (Lista2* ini, int remov) {
 
-    List* P = ini;
+    Lista2* P = ini;
 
-    while (p != NULL){
+    while (P != NULL){
 
-        if (P -> num == remov) return P;
+        if (P -> num == remov){
+
+            printf ("Inteiro localizado\n");
+            return P;
+        }
+        
 
         P = P -> prox;
     }
+
+    printf ("Inteiro não localizado\n");
+
     return NULL;
   }
 
@@ -61,7 +106,7 @@ Lista2* Localiza (Lista2* ini, int remov) {
         P -> ant -> prox = P -> prox; 
 
     if ((P -> prox) != NULL) 
-        P -> prox -> ant = p->ant;
+        P -> prox -> ant = P -> ant;
     
     free(P);
     
